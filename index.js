4a6704652d096c84385c64d353e3452b44095c99
Rudra Rakesh Limbachiya <rudralimbachiya10@gmail.com>
2026-07-18 22:06:54 +0530

    Stage 2: read endpoints with 404

--- index.js
+++ index.js
@@ -2,12 +2,34 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const tasks = [
+    { id: 1, title: 'learn express.js', completed: true },
+    { id: 2, title: 'complete assignment', completed: true },
+    { id: 3, title: 'watch FIFA finals', completed: false }
+];
+
 app.get('/',(req,res) =>{
     res.json({"name": "Task API", "version": "1.0", "endpoints": ["/tasks"]});
 });
 
 app.get('/health',(req,res)=>{
     res.json({"status": "ok"});
+});
+
+app.get ('/tasks', (req,res) =>{
+    res.json(tasks);
+});
+
+app.get('/tasks/:id', (req, res) => {
+  const taskId = parseInt(req.params.id);
+
+  const task = tasks.find(t => t.id === taskId);
+
+  if (!task) {
+    return res.status(404).json({ "error": `Task ${taskId} not found` });
+  }
+
+  res.json(task);
 });
 
 app.listen(port, () => {
